4f4f79fd4984d675e269d971abd1418a07ed88e1
alessandromarcellini <alessandromarcellini49@gmail.com>
2022-11-04 21:16:22 +0100

    fixata logica della briscola e creato sistema di check winner

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import random
 
 class Card:
-    def __init__(self, valore, seme):
-        self.valore = valore
-        self.seme = seme
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
 
     def __str__(self):
-        return f"{self.valore} di {self.seme}"
+        return f"{self.value} di {self.suit}"
 
 
 #40 carte: denari, spade, coppe e bastoni
@@ -16,11 +16,11 @@ class Deck:
         self.cards = []
 
     def create(self):
-        semi = ["denari", "spade", "coppe", "bastoni"]
+        suits = ["denari", "spade", "coppe", "bastoni"]
 
-        for seme in semi:
-            for valore in range(1, 11):
-                self.cards.append(Card(valore, seme))
+        for suit in suits:
+            for value in range(1, 11):
+                self.cards.append(Card(value, suit))
 
     def showDeck(self):
         for card in self.cards:
@@ -61,6 +61,7 @@ class Player:
 
     def bot_play(self):
         randint = random.randint(0, 2)
+        print(f"Il Bot ha giocato {self.hand[randint]}")
         return self.hand.pop(randint)
 
     def draw(self, deck):
@@ -74,11 +75,42 @@ class Player:
 
 class Table:
     def __init__(self):
-        semi = ["denari", "spade", "coppe", "bastoni"]
 
         self.cards = []
-        self.briscola = semi[random.randint(0, 3)]
 
+    def get_briscola(self, deck): #mette la briscola in self.briscola e la sposta dalla cima del mazzo al fondo
+        self.briscola = deck.cards.pop()
+        deck.cards.append(self.briscola)
+
+    def check_winner(self): #return 1 se vince c2. return 0 se vince c1
+        hierarchy = [2, 4, 5, 6, 7, 8, 9, 10, 3, 1]
+
+        if str(self.cards[0].suit) == str(self.briscola) and str(self.cards[1].suit) != str(self.briscola):
+            print("\n\n vince il PRIMO ad aver giocato \n\n")
+            return 0 #vince il primo ad aver giocato
+        elif str(self.cards[1].suit) == str(self.briscola) and str(self.cards[0].suit) != str(self.briscola):
+            print("\n\n vince il SECONDO ad aver giocato \n\n")
+            return 1 #vince il secondo ad aver giocato
+        else: #nessuna delle 2 è briscola o entrambe lo sono
+            if str(self.cards[0].suit) != str(self.cards[1].suit):
+                print("\n\n vince il PRIMO ad aver giocato \n\n")
+                return 0 #vince il primo ad aver giocato
+            else: #se hanno stesso seme
+                if hierarchy.index(int(self.cards[0].value)) > hierarchy.index(int(self.cards[1].value)):
+                    print("\n\n vince il PRIMO ad aver giocato \n\n")
+                    return 0 #vince il primo ad aver giocato
+                else:
+                    print("\n\n vince il SECONDO ad aver giocato \n\n")
+                    return 1 #vince il secondo ad aver giocato
+
+
+    def clean(self):
+        return self.cards.clear()
+
+    def display(self):
+        player.showHand()
+        print("\n-----------\n")
+        cpu.showHand()
 
 
 #creazione giocatori
@@ -88,14 +120,16 @@ cpu = Player("Cpu")
 
 #creazione tavolo
 table = Table()
-print("Buona fortuna!")
-print(f"La briscola è: {table.briscola}\n")
-
 
 #creazione mazzo
 deck = Deck()
 deck.create()
 deck.shuffle()
+
+print("Buona fortuna!")
+#creazione briscola
+table.get_briscola(deck)
+print(f"La briscola è: {table.briscola}\n")
 
 #creazione mazzo di riferimento, ogni volta che esce una carta la leviamo, teniamo traccia delle carte uscite
 reference_deck = Deck()
@@ -107,25 +141,55 @@ reference_deck.create()
 player.buildHand(deck)
 cpu.buildHand(deck)
 
+winner = 0
+starting = 0
 while len(deck.cards) != 0:
     # mostra mano
-    player.showHand()
-    print("\n-----------\n")
-    cpu.showHand()
-
-
+    table.display()
     #scegli una carta e mettila sul tavolo
-    table.cards.append(player.play())
+    if winner != 0:
+        if starting == 0:
+            starting = 1
+        else:
+            starting = 0
 
-    #logica bot...
-    table.cards.append(cpu.bot_play())
+    if starting == 0: #inizia il giocatore
+        table.cards.append(player.play())
+        table.cards.append(cpu.bot_play()) #logica bot...
+
+    else: #inizia la cpu
+        table.cards.append(cpu.bot_play()) #logica bot...
+        table.cards.append(player.play())
+
 
     #scegli vincitore
-
+    winner = table.check_winner()
+    if winner != 0: #ha vinto il secondo a giocare => switch di starting
+        if starting == 0: #se era il player a iniziare adesso sarà la cpu
+            #pescano
+            cpu.draw(deck)
+            player.draw(deck)
+        else: #se era la cpu a iniziare adesso sarà il player
+            #pescano
+            player.draw(deck)
+            cpu.draw(deck)
+    else:
+        if starting == 0:
+            cpu.draw(deck)
+            player.draw(deck)
+        else:
+            player.draw(deck)
+            cpu.draw(deck)
 
     #assegna punti al vincitore
 
     #pescare carta
 
-    #riniziare
 
+
+    #riniziare
+    table.clean()
+
+
+
+#fixato briscola e sistema check winner
